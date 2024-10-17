51e6363d06781c61434f59cf6e2e65eee9dfcc51
Dilan <dilan.palafox.12@gmail.com>
2024-10-16 19:32:00 -0600

    Subida de gestor de API's
    Se implementa el codigo en c# para generar las api en el servidor

---
+++ BackEnd/API's/proyecto/BackEnd-ArtePuebla/Functions/SQL_User.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+
+using System.Data;
+
+namespace BackEnd_ArtePuebla.Functions
+{
+    public class SQL_User
+    {
+        string cadenaSQL = ""; //Cadena en proceso...
+        public bool ValidUser(string email, string pass)
+        {
+            try
+            {
+                using MySqlConnection conn = new(cadenaSQL);
+                conn.Open();
+                using MySqlCommand cmd = new()
+                {
+                    Connection = conn,
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = "LoginUser"
+                };
+                cmd.Parameters.AddWithValue("email", email);
+                cmd.Parameters.AddWithValue("pass", pass);
+                string? exist = cmd.ExecuteScalar().ToString();
+                return !string.IsNullOrWhiteSpace(exist);
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine($"Error en el metodo ValidUser {ex}");
+                return false;
+            }
+        }
+    }
+}
